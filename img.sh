ec461e0ae267f9b487813799eaa1b75de7f65181
Bruno Sabença <brunosabenca@users.noreply.github.com>
2016-05-26 00:46:07 +0100

    Changes scrot to maim
    https://github.com/naelstrof/maim

--- img.sh
+++ img.sh
@@ -5,7 +5,7 @@
 clientid='3e7a4deb7ac67da'
 img=$(mktemp '/tmp/img-XXXXXX.png')
 
-scrot -z "$@" $img >/dev/null 2>&1 || exit
+maim "$@" $img >/dev/null 2>&1 || exit
 res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$img" "https://api.imgur.com/3/upload")
 
 echo $res | grep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
